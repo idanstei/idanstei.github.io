fbc3f1975326a5a0f7bf3e98ca09131952bcad7e
Idan Steinberg <idanstei@stanford.edu>
2026-08-15 17:23:37 +0000

    Fix Google Analytics consent handling

--- _plugins/al_cookie_consent_fix.rb
+++ _plugins/al_cookie_consent_fix.rb
@@ -7,10 +7,19 @@ module AlCookie
     alias_method :render_setup_script_without_consent_fix, :render_setup_script
 
     def render_setup_script(context)
-      render_setup_script_without_consent_fix(context).sub(
+      script = render_setup_script_without_consent_fix(context)
+
+      script = script.sub(
         "onFirstConsent: function (consentData)",
         "onConsent: function (consentData)"
       )
+
+      script = script.sub(
+        "var categories = consentData.categories || consentData;",
+        'var categories = { analytics: window.CookieConsent.acceptedCategory("analytics") };'
+      )
+
+      script
     end
   end
 end
